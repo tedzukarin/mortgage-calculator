81a2213c7fead3ed4b29816a6a549b1554c5577b
tedzukarin <tedzukarin@yandex.ru>
2026-03-27 15:54:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
 
 if __name__ == '__main__':
     # Для разработки
-    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
     # Для продакшена закомментируйте строку выше и раскомментируйте эту:
-    app.run(host='0.0.0.0', port=5000)
+    # app.run(host='0.0.0.0', port=5000)
